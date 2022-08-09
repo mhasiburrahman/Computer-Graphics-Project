7b10abe1f0c0e2d06ce941047fe65a9d6d2c47f3
Hasan Ahamed <officials.hasan@gmail.com>
2022-08-09 22:23:50 +0600

    added second bird

--- test/models/Bird.h
+++ test/models/Bird.h
@@ -106,6 +106,105 @@ private:
         glFlush();
     }
 
+    void bird2()
+    {
+
+        glPushMatrix();
+        glTranslatef(positionBird2, 0.0f, 0.0f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3ub(248, 196, 113);
+        glVertex2f(0.57f, 0.62f);
+        glVertex2f(0.52f, 0.63f);
+        glVertex2f(0.57f, 0.6f);
+        glEnd();
+
+        glBegin(GL_TRIANGLES);
+        glColor3ub(34, 153, 84);
+        glVertex2f(0.65f, 0.61f);
+        glVertex2f(0.65f, 0.63f);
+        glVertex2f(0.67f, 0.61f);
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3ub(44, 62, 80);
+        glVertex2f(0.57f, 0.56f);
+        glVertex2f(0.59f, 0.57f);
+        glEnd();
+        glBegin(GL_LINES);
+        glColor3ub(44, 62, 80);
+        glVertex2f(0.59f, 0.55f);
+        glVertex2f(0.61f, 0.57f);
+        glEnd();
+
+        int i2;
+        GLfloat x2 = .6f;
+        GLfloat y2 = .6f;
+        GLfloat radius2 = .04f;
+        int triangleAmount2 = 20;
+
+        GLfloat twicePi2 = 2.0f * 3.14;
+
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(213, 244, 13);
+        glVertex2f(x2, y2);
+        for (i2 = 0; i2 <= triangleAmount2; i2++)
+        {
+            glVertex2f(
+                x2 + (radius2 * cos(i2 * twicePi2 / triangleAmount2)),
+                y2 + (radius2 * sin(i2 * twicePi2 / triangleAmount2)));
+        }
+        glEnd();
+
+        int i12;
+        GLfloat x12 = .64f;
+        GLfloat y12 = .62f;
+        GLfloat radius12 = .02f;
+        int triangleAmount12 = 20;
+
+        GLfloat twicePi12 = 2.0f * 3.14;
+
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(243, 156, 18);
+        glVertex2f(x12, y12);
+        for (i12 = 0; i12 <= triangleAmount12; i12++)
+        {
+            glVertex2f(
+                x12 + (radius12 * cos(i12 * twicePi12 / triangleAmount12)),
+                y12 + (radius12 * sin(i12 * twicePi12 / triangleAmount12)));
+        }
+        glEnd();
+
+        int i112;
+        GLfloat x112 = .65f;
+        GLfloat y112 = .62f;
+        GLfloat radius112 = .005f;
+        int triangleAmount112 = 20;
+
+        GLfloat twicePi112 = 2.0f * 3.14;
+
+        glBegin(GL_TRIANGLE_FAN);
+        glColor3ub(39, 55, 70);
+        glVertex2f(x112, y112);
+        for (i112 = 0; i112 <= triangleAmount112; i112++)
+        {
+            glVertex2f(
+                x112 + (radius112 * cos(i112 * twicePi112 / triangleAmount112)),
+                y112 + (radius112 * sin(i112 * twicePi112 / triangleAmount112)));
+        }
+        glEnd();
+
+        glBegin(GL_TRIANGLES);
+        glColor3ub(248, 196, 113);
+        glVertex2f(0.6f, 0.62f);
+        glVertex2f(0.55f, 0.65f);
+        glVertex2f(0.6f, 0.6f);
+        glEnd();
+
+        glPopMatrix();
+        glFlush();
+    }
+
 public:
     GLfloat getPositionBird1()
     {
